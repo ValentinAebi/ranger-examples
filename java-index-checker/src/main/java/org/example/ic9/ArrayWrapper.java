3f52ab76f24f091ac3883196488d8bbec315f01c
Valentin Aebi <vaaebi@gmail.com>
2026-06-15 11:45:43 +0200

    Remove `Nullable` annotations

--- java-index-checker/src/main/java/org/example/ic9/ArrayWrapper.java
+++ java-index-checker/src/main/java/org/example/ic9/ArrayWrapper.java
@@ -1,21 +1,14 @@
 package org.example.ic9;
 
-/*
- * Note: in comparison to the original version of this example found in the manual of the Checker Framework, 
- * this example has been slightly modified by adding @Nullable annotations (without which the Checker Framework 
- * rejects the code because of nullability issues).
- */
-
 import org.checkerframework.checker.index.qual.SameLen;
 import org.checkerframework.checker.index.qual.IndexFor;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.index.qual.LengthOf;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 
 /** ArrayWrapper is a fixed-size generic collection. */
 public class ArrayWrapper<T> {
-    private final @Nullable Object @SameLen("this") [] delegate;
+    private final Object @SameLen("this") [] delegate;
 
     @SuppressWarnings("index") // constructor creates object of size @SameLen(this) by definition
     ArrayWrapper(@NonNegative int size) {
@@ -26,12 +19,12 @@ public class ArrayWrapper<T> {
         return delegate.length;
     }
 
-    public void set(@IndexFor("this") int index, @Nullable T obj) {
+    public void set(@IndexFor("this") int index, T obj) {
         delegate[index] = obj;
     }
 
     @SuppressWarnings("unchecked") // required for normal Java compilation due to unchecked cast
-    public @Nullable T get(@IndexFor("this") int index) {
+    public T get(@IndexFor("this") int index) {
         return (T) delegate[index];
     }
 }
